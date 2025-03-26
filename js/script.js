9d8af856f91ea2cc5532cb0587637f0d9f92c102
sourabhsam123 <63491820+sourabhsam123@users.noreply.github.com>
2025-03-26 17:07:36 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const headerBackground = document.querySelector('.header-background');
     const navLinks = document.querySelectorAll('.navbar a');
     const missionText = document.getElementById('mission-text');
     const modal = document.getElementById('imageModal');
-
-    function changeHeaderBackground(imageName) {
-        if (headerBackground) {
-            headerBackground.style.backgroundImage = `url('images/${imageName}')`;
-        }
-    }
 
     function changeNavColor(color) {
         navLinks.forEach(link => {
@@ -19,9 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateHeaderFromActiveLink() {
         const activeLink = document.querySelector('.navbar a.active');
         if (activeLink) {
-            const initialImage = activeLink.getAttribute('data-image');
-            changeHeaderBackground(initialImage);
-
             const initialMission = activeLink.getAttribute('data-mission');
             if (missionText) missionText.textContent = initialMission;
 
@@ -31,6 +21,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     navLinks.forEach(link => {
+        let originalText = link.textContent;
+        let englishText = link.getAttribute("data-english");
+
+        // Click Event - Active Link & Mission Text
         link.addEventListener('click', function(event) {
             event.preventDefault();
 
@@ -39,9 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
             });
 
             this.classList.add('active');
-
-            const imageName = this.getAttribute('data-image');
-            changeHeaderBackground(imageName);
 
             const mission = this.getAttribute('data-mission');
             if (missionText) missionText.textContent = mission;
@@ -59,6 +50,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 window.location.href = href;
             }
         });
+
+        // Hover Effect - Change Text to english
+        link.addEventListener("mouseenter", function() {
+            this.textContent = englishText;
+        });
+
+        link.addEventListener("mouseleave", function() {
+            this.textContent = originalText;
+        });
     });
 
     // Ensure the correct header is set on page load
@@ -74,12 +74,10 @@ document.addEventListener('DOMContentLoaded', function() {
     window.closeModal = function() { 
         document.getElementById('imageModal').classList.add('hidden');
     };
-    // Close modal if user clicks outside the image
-    
+
     // Close modal if user clicks outside the image
     if (modal) {
         modal.addEventListener('click', function(event) {
-            // Close only if the user clicks outside the image (not inside the image)
             if (event.target === modal) {
                 closeModal();
             }
